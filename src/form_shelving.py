5b297bf77899a958e5a6fc1a88ff4dc2a2d91903
SchokiCoder <94720979+SchokiCoder@users.noreply.github.com>
2021-12-01 22:16:50 +0100

    Add files via upload
    fix license

--- src/form_shelving.py
+++ src/form_shelving.py
@@ -1,17 +1,18 @@
-#Troll ERP
-#Copyright (C) 2021 Andy Frank Schoknecht
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, only version 3 of the License.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    Troll ERP
+#    Copyright (C) 2021  Andy Frank Schoknecht
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import tkinter
 import tkinter.messagebox
