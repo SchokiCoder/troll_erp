ec7d38452dafec619940a537fa3733c4edc6565f
SchokiCoder <afschoknecht@gmail.com>
2022-12-10 12:16:19 +0100

    change in-app license info

--- src/constants.py
+++ src/constants.py
@@ -21,19 +21,21 @@ APP_MAJOR = 1
 APP_MINOR = 0
 APP_PATCH = 0
 
-APP_LICENSE = """Copyright (C) 2021 Andy Frank Schoknecht
+APP_LICENSE = """Copyright (C) 2021 - 2022  Andy Frank Schoknecht
 
-This program is free software: you can redistribute it and/or modify
+This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, only version 3 of the License.
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+You should have received a copy of the GNU General Public License along
+with this program; if not see
+<https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>."""
 
 #path defintions
 PATH_DB = "troll.db"
